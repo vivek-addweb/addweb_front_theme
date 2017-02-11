a3c2345c1057f6f18cb9a33f96c4579de647b031
avani-addweb <avani.addweb@gmail.com>
2017-02-11 17:36:09 +0530

    Changed in home page slider design

--- js/animate-slide.js
+++ js/animate-slide.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
   var window_width = jQuery(window).width();
-  jQuery('.aws-controls .aws-prev-link').addClass('hide-arrow');
+  jQuery('.addweb-controls .addweb-prev-link').addClass('hide-arrow');
   
   // Animate slide for Desktop & Mobile
   slider_desktop ();
@@ -9,38 +9,38 @@ jQuery(document).ready(function() {
   jQuery(window).resize(function() {
     window_width = jQuery(window).width();
     if (window_width > 767) {
-      jQuery('.aws-slider .aws-row.active .aws-col').removeClass('active');
+      jQuery('.addweb-slider .addweb-row.active .addweb-col').removeClass('active');
 
       // Resize
-      jQuery('.aws-slider').css('height', 'auto');
-      jQuery('.aws-slider .aws-row .aws-col').css('height', 'auto');
-      var aws_col_h_max = 0;
-      jQuery('.aws-slider .aws-row').each(function() {
-        var awscolHeight = jQuery(this).outerHeight();
-        if (awscolHeight > aws_col_h_max) {
-          aws_col_h_max = awscolHeight;
+      jQuery('.addweb-slider').css('height', 'auto');
+      jQuery('.addweb-slider .addweb-row .addweb-col').css('height', 'auto');
+      var addweb_col_h_max = 0;
+      jQuery('.addweb-slider .addweb-row').each(function() {
+        var addwebcolHeight = jQuery(this).outerHeight();
+        if (addwebcolHeight > addweb_col_h_max) {
+          addweb_col_h_max = addwebcolHeight;
         }
       });
-      jQuery('.aws-slider').css('height', aws_col_h_max);
-      jQuery('.aws-slider .aws-row .aws-col').css('height', aws_col_h_max);
+      jQuery('.addweb-slider').css('height', addweb_col_h_max);
+      jQuery('.addweb-slider .addweb-row .addweb-col').css('height', addweb_col_h_max);
       // Resize End
     }
     else if (window_width < 768) {
       //slider_mobile (window_width);
-      jQuery('.aws-slider .aws-row.active .aws-col-1').addClass('active');
+      jQuery('.addweb-slider .addweb-row.active .addweb-col-1').addClass('active');
 
       // Resize
-      jQuery('.aws-slider').css('height', 'auto');
-      jQuery('.aws-slider .aws-row .aws-col').css('height', 'auto');
-      var aws_col_h_max = 0;
-      jQuery('.aws-slider .aws-row').each(function() {
-        var awscolHeight = jQuery(this).children('.aws-col').outerHeight();
-        if (awscolHeight > aws_col_h_max) {
-          aws_col_h_max = awscolHeight;
+      jQuery('.addweb-slider').css('height', 'auto');
+      jQuery('.addweb-slider .addweb-row .addweb-col').css('height', 'auto');
+      var addweb_col_h_max = 0;
+      jQuery('.addweb-slider .addweb-row').each(function() {
+        var addwebcolHeight = jQuery(this).children('.addweb-col').outerHeight();
+        if (addwebcolHeight > addweb_col_h_max) {
+          addweb_col_h_max = addwebcolHeight;
         }
       });
-      jQuery('.aws-slider').css('height', aws_col_h_max);
-      jQuery('.aws-slider .aws-row .aws-col').css('height', aws_col_h_max);
+      jQuery('.addweb-slider').css('height', addweb_col_h_max);
+      jQuery('.addweb-slider .addweb-row .addweb-col').css('height', addweb_col_h_max);
       // Resize End
     }
   });
@@ -48,121 +48,121 @@ jQuery(document).ready(function() {
 
   // Set height for Col FOR DESKTOP
   if (jQuery(window).width() > 767) {
-    var aws_col_h_max = 0;
-    jQuery('.aws-slider .aws-row').each(function() {
-      var awscolHeight = jQuery(this).outerHeight();
-      if (awscolHeight > aws_col_h_max) {
-        aws_col_h_max = awscolHeight;
+    var addweb_col_h_max = 0;
+    jQuery('.addweb-slider .addweb-row').each(function() {
+      var addwebcolHeight = jQuery(this).outerHeight();
+      if (addwebcolHeight > addweb_col_h_max) {
+        addweb_col_h_max = addwebcolHeight;
       }
     });
-    jQuery('.aws-slider').css('height', aws_col_h_max);
-    jQuery('.aws-slider .aws-row .aws-col').css('height', aws_col_h_max);
+    jQuery('.addweb-slider').css('height', addweb_col_h_max);
+    jQuery('.addweb-slider .addweb-row .addweb-col').css('height', addweb_col_h_max);
   }
 
   // Set height for Col FOR MOBILE
   if (window_width < 768) {
-    var aws_col_h_max = 0;
-    jQuery('.aws-slider .aws-row').each(function() {
-      var awscolHeight = jQuery(this).children('.aws-col').outerHeight();
-      if (awscolHeight > aws_col_h_max) {
-        aws_col_h_max = awscolHeight;
+    var addweb_col_h_max = 0;
+    jQuery('.addweb-slider .addweb-row').each(function() {
+      var addwebcolHeight = jQuery(this).children('.addweb-col').outerHeight();
+      if (addwebcolHeight > addweb_col_h_max) {
+        addweb_col_h_max = addwebcolHeight;
       }
     });
-    jQuery('.aws-slider').css('height', aws_col_h_max);
-    jQuery('.aws-slider .aws-row .aws-col').css('height', aws_col_h_max);
+    jQuery('.addweb-slider').css('height', addweb_col_h_max);
+    jQuery('.addweb-slider .addweb-row .addweb-col').css('height', addweb_col_h_max);
   }
 });
 
 // For Desktop
 function slider_desktop () {
-  jQuery('.aws-slider .aws-row:nth-child(1)').addClass('active');
+  jQuery('.addweb-slider .addweb-row:nth-child(1)').addClass('active');
   // For Prev
-  jQuery('.aws-controls .aws-prev-link').click(function() {
+  jQuery('.addweb-controls .addweb-prev-link').click(function() {
     window_width = jQuery(window).width();
     if (window_width > 767) {
-      if (jQuery('.aws-slider .aws-row.active').is(':first-child')) {
+      if (jQuery('.addweb-slider .addweb-row.active').is(':first-child')) {
         // To detect first row
       }
-      else if (jQuery('.aws-slider .aws-row:nth-child(2)').hasClass('active')) {
-        jQuery('.aws-controls .aws-prev-link').addClass('hide-arrow');
-        jQuery('.aws-slider .aws-row.active').removeClass('active').prev().addClass('active');
+      else if (jQuery('.addweb-slider .addweb-row:nth-child(2)').hasClass('active')) {
+        jQuery('.addweb-controls .addweb-prev-link').addClass('hide-arrow');
+        jQuery('.addweb-slider .addweb-row.active').removeClass('active').prev().addClass('active');
       }
       else {
-        jQuery('.aws-controls .aws-prev-link').removeClass('hide-arrow');
-        jQuery('.aws-controls .aws-next-link').removeClass('hide-arrow');
-        jQuery('.aws-slider .aws-row.active').removeClass('active').prev().addClass('active');
+        jQuery('.addweb-controls .addweb-prev-link').removeClass('hide-arrow');
+        jQuery('.addweb-controls .addweb-next-link').removeClass('hide-arrow');
+        jQuery('.addweb-slider .addweb-row.active').removeClass('active').prev().addClass('active');
       }
     }
   });
   // For Next
-  jQuery('.aws-controls .aws-next-link').click(function() {
+  jQuery('.addweb-controls .addweb-next-link').click(function() {
     window_width = jQuery(window).width();
     if (window_width > 767) {
-      if (jQuery('.aws-slider .aws-row:nth-last-child(1)').hasClass('active')) {
+      if (jQuery('.addweb-slider .addweb-row:nth-last-child(1)').hasClass('active')) {
         // To detect last row
       }
-      else if (jQuery('.aws-slider .aws-row:nth-last-child(2)').hasClass('active')) {
-        jQuery('.aws-controls .aws-next-link').addClass('hide-arrow');
-        jQuery('.aws-slider .aws-row.active').removeClass('active').next().addClass('active');
+      else if (jQuery('.addweb-slider .addweb-row:nth-last-child(2)').hasClass('active')) {
+        jQuery('.addweb-controls .addweb-next-link').addClass('hide-arrow');
+        jQuery('.addweb-slider .addweb-row.active').removeClass('active').next().addClass('active');
       }
       else {
-        jQuery('.aws-controls .aws-prev-link').removeClass('hide-arrow');
-        jQuery('.aws-slider .aws-row.active').removeClass('active').next().addClass('active');
+        jQuery('.addweb-controls .addweb-prev-link').removeClass('hide-arrow');
+        jQuery('.addweb-slider .addweb-row.active').removeClass('active').next().addClass('active');
       }
     }
   });
 }
 // For Mobile
 function slider_mobile () {
-  jQuery('.aws-slider .aws-row:nth-child(1)').addClass('active');
-  jQuery('.aws-slider .aws-row:nth-child(1) .aws-col-1').addClass('active');
+  jQuery('.addweb-slider .addweb-row:nth-child(1)').addClass('active');
+  jQuery('.addweb-slider .addweb-row:nth-child(1) .addweb-col-1').addClass('active');
   // For Prev
-  jQuery('.aws-controls .aws-prev-link').click(function() {
+  jQuery('.addweb-controls .addweb-prev-link').click(function() {
     window_width = jQuery(window).width();
     if (window_width < 768) {
-      jQuery('.aws-controls .aws-next-link').removeClass('hide-arrow');
-      if (jQuery('.aws-slider .aws-row .aws-col-2.active').hasClass('active')) {
-        jQuery('.aws-slider .aws-row .aws-col.active').removeClass('active').prev().addClass('active');
-        if (jQuery('.aws-slider .aws-row:first-child').hasClass('active')) {
-          jQuery('.aws-controls .aws-prev-link').addClass('hide-arrow');
+      jQuery('.addweb-controls .addweb-next-link').removeClass('hide-arrow');
+      if (jQuery('.addweb-slider .addweb-row .addweb-col-2.active').hasClass('active')) {
+        jQuery('.addweb-slider .addweb-row .addweb-col.active').removeClass('active').prev().addClass('active');
+        if (jQuery('.addweb-slider .addweb-row:first-child').hasClass('active')) {
+          jQuery('.addweb-controls .addweb-prev-link').addClass('hide-arrow');
         }
       }
-      else if (jQuery('.aws-slider .aws-row .aws-col-1.active').hasClass('active')) {
-        if (jQuery('.aws-slider .aws-row:first-child').hasClass('active')) {
-          jQuery('.aws-slider .aws-row:first-child').addClass('active');
+      else if (jQuery('.addweb-slider .addweb-row .addweb-col-1.active').hasClass('active')) {
+        if (jQuery('.addweb-slider .addweb-row:first-child').hasClass('active')) {
+          jQuery('.addweb-slider .addweb-row:first-child').addClass('active');
         }
         else {
-          jQuery('.aws-slider .aws-row .aws-col.active').parent('.aws-row').removeClass('active').prev().addClass('active');
-          jQuery('.aws-slider .aws-row .aws-col').removeClass('active');
-          jQuery('.aws-slider .aws-row.active .aws-col-2').addClass('active');
+          jQuery('.addweb-slider .addweb-row .addweb-col.active').parent('.addweb-row').removeClass('active').prev().addClass('active');
+          jQuery('.addweb-slider .addweb-row .addweb-col').removeClass('active');
+          jQuery('.addweb-slider .addweb-row.active .addweb-col-2').addClass('active');
         }
       }
     }
   });
   // For Next
-  jQuery('.aws-controls .aws-next-link').click(function() {
+  jQuery('.addweb-controls .addweb-next-link').click(function() {
     window_width = jQuery(window).width();
     if (window_width < 768) {
-      if (jQuery('.aws-slider .aws-row .aws-col-1.active').hasClass('active')) {
-        if(jQuery('.aws-slider .aws-row:last-child .aws-col').length == '2' && jQuery('.aws-slider .aws-row:last-child').hasClass('active')) {
-          jQuery('.aws-controls .aws-next-link').addClass('hide-arrow');
+      if (jQuery('.addweb-slider .addweb-row .addweb-col-1.active').hasClass('active')) {
+        if(jQuery('.addweb-slider .addweb-row:last-child .addweb-col').length == '2' && jQuery('.addweb-slider .addweb-row:last-child').hasClass('active')) {
+          jQuery('.addweb-controls .addweb-next-link').addClass('hide-arrow');
         }
-        if (jQuery('.aws-slider .aws-row.active .aws-col-2').length) {
-          jQuery('.aws-controls .aws-prev-link').removeClass('hide-arrow');
-          jQuery('.aws-slider .aws-row .aws-col.active').removeClass('active').next().addClass('active');
+        if (jQuery('.addweb-slider .addweb-row.active .addweb-col-2').length) {
+          jQuery('.addweb-controls .addweb-prev-link').removeClass('hide-arrow');
+          jQuery('.addweb-slider .addweb-row .addweb-col.active').removeClass('active').next().addClass('active');
         }
       }
-      else if (jQuery('.aws-slider .aws-row .aws-col-2.active').hasClass('active')) {
-        if (jQuery('.aws-slider .aws-row:last-child .aws-col').length == '1' && jQuery('.aws-slider .aws-row:nth-last-child(2)').hasClass('active')) {
-          jQuery('.aws-controls .aws-next-link').addClass('hide-arrow');
+      else if (jQuery('.addweb-slider .addweb-row .addweb-col-2.active').hasClass('active')) {
+        if (jQuery('.addweb-slider .addweb-row:last-child .addweb-col').length == '1' && jQuery('.addweb-slider .addweb-row:nth-last-child(2)').hasClass('active')) {
+          jQuery('.addweb-controls .addweb-next-link').addClass('hide-arrow');
         }
-        if (jQuery('.aws-slider .aws-row:last-child').hasClass('active')) {
+        if (jQuery('.addweb-slider .addweb-row:last-child').hasClass('active')) {
           // To detect last row
         }
         else {
-          jQuery('.aws-slider .aws-row .aws-col.active').parent('.aws-row').removeClass('active').next().addClass('active');
-          jQuery('.aws-slider .aws-row .aws-col').removeClass('active');
-          jQuery('.aws-slider .aws-row.active .aws-col-1').addClass('active');
+          jQuery('.addweb-slider .addweb-row .addweb-col.active').parent('.addweb-row').removeClass('active').next().addClass('active');
+          jQuery('.addweb-slider .addweb-row .addweb-col').removeClass('active');
+          jQuery('.addweb-slider .addweb-row.active .addweb-col-1').addClass('active');
         }
       }
     }

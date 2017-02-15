45a7b3fba6efc33385612b0e487be4dd4d8320f6
hiral-addweb <hiral.addweb@gmail.com>
2017-02-15 10:58:43 +0530

    Design of header

--- js/theme.js
+++ js/theme.js
@@ -1,159 +1,179 @@
-// jQuery(document).ready(function() {
+jQuery(document).ready(function() {
 
-//   // Banner - Slider
-//   jQuery('.banner .banner-slider').owlCarousel({
-//     loop: false,
-//     margin: 0,
-//     nav: true,
-//     autoplay: false,
-//     dots: true,
-//     lazyLoad: true,
-//     autoplayTimeout: 2000,
-//     autoWidth: false,
-//     slideBy: 1,
-//     items:1
-//   });
-
-
-//   // Projects Slider
-//   jQuery('.project-list .project-list-grid').owlCarousel({
-//     loop: true,
-//     margin: 0,
-//     nav: true,
-//     autoplay: true,
-//     dots: true,
-//     lazyLoad: true,
-//     autoplayTimeout: 2000,
-//     autoWidth: false,
-//     slideBy: 1,
-//     items:5,
-//     responsive:{
-//       320:{
-//         items:1
-//       },
-//       480:{
-//         items:2
-//       },
-//       767:{
-//         items:3
-//       },
-//       1000:{
-//         items:5
-//       }
-//     }
-//   });
-
-//   // Blog Slider
-//   jQuery('.latest-blog .latest-blog-slider').owlCarousel({
-//     loop: true,
-//     margin: 20,
-//     nav: true,
-//     autoplay: true,
-//     dots: false,
-//     lazyLoad: true,
-//     autoplayTimeout: 2000,
-//     autoWidth: false,
-//     slideBy: 1,
-//     items:3,
-//     responsive:{
-//       320:{
-//         items:1
-//       },
-//       480:{
-//         items:2
-//       },
-//       767:{
-//         items:2
-//       },
-//       1000:{
-//         items:3
-//       }
-//     }
-//   });
+  // Banner - Slider
+  jQuery('.banner .banner-slider').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    autoplay: true,
+    dots: true,
+    lazyLoad: true,
+    autoplayTimeout: 2000,
+    autoWidth: false,
+    slideBy: 1,
+    items:1
+  });
 
 
-//   // Client Slider
-//   jQuery('.our-client .our-client-slider').owlCarousel({
-//     loop: true,
-//     margin: 0,
-//     nav: true,
-//     autoplay: true,
-//     dots: false,
-//     lazyLoad: true,
-//     autoplayTimeout: 2000,
-//     autoWidth: false,
-//     slideBy: 1,
-//     items:4,
-//     responsive:{
-//       320:{
-//         items:1
-//       },
-//       480:{
-//         items:2
-//       },
-//       767:{
-//         items:3
-//       },
-//       1000:{
-//         items:4
-//       }
-//     }
-//   });
+  // Projects Slider
+  jQuery('.project-list .project-list-grid').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    autoplay: true,
+    dots: true,
+    lazyLoad: true,
+    autoplayTimeout: 2000,
+    autoWidth: false,
+    slideBy: 1,
+    items:5,
+    responsive:{
+      320:{
+        items:1
+      },
+      480:{
+        items:2
+      },
+      767:{
+        items:3
+      },
+      1000:{
+        items:5
+      }
+    }
+  });
+
+  // Blog Slider
+  jQuery('.latest-blog .latest-blog-slider').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    autoplay: true,
+    dots: false,
+    lazyLoad: true,
+    autoplayTimeout: 2000,
+    autoWidth: false,
+    slideBy: 1,
+    items:3,
+    responsive:{
+      320:{
+        items:1
+      },
+      480:{
+        items:2
+      },
+      767:{
+        items:2
+      },
+      1000:{
+        items:3
+      }
+    }
+  });
 
 
-//   // Testimonial Slider
-//   jQuery('.about-us .about-us-testimonial .testimonial-slider').owlCarousel({
-//     loop: true,
-//     margin: 0,
-//     nav: false,
-//     autoplay: true,
-//     dots: true,
-//     lazyLoad: true,
-//     autoplayTimeout: 2000,
-//     autoWidth: false,
-//     slideBy: 1,
-//     items:1
-//   });
-
-//   // Off-Canvas Navigation
-//   jQuery('.menu-offset-left').prepend('<a href="javascript:void(0);" class="aws-close-btn">X</a>');
-
-//   jQuery('.toggle-menu').on('click', function(event) {
-//     event.stopPropagation();
-//     jQuery('.menu-offset-left').toggleClass('fade-in');
-//   });
-//   jQuery('.aws-close-btn').click(function() {
-//     // event.preventDefault();
-//     jQuery(this).parent().removeClass('fade-in');
-//   });
-//   // jQuery(document).click(function() {
-//   //   // alert('test');
-//   //   jQuery('.menu-offset-left').removeClass('fade-in');
-//   // });
+  // Client Slider
+  jQuery('.our-client .our-client-slider').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    autoplay: true,
+    dots: false,
+    lazyLoad: true,
+    autoplayTimeout: 2000,
+    autoWidth: false,
+    slideBy: 1,
+    items:4,
+    responsive:{
+      320:{
+        items:1
+      },
+      480:{
+        items:2
+      },
+      767:{
+        items:3
+      },
+      1000:{
+        items:4
+      }
+    }
+  });
 
 
-//   // Submenu visible on click
+  // Testimonial Slider
+  jQuery('.about-us .about-us-testimonial .testimonial-slider').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    autoplay: true,
+    dots: true,
+    lazyLoad: true,
+    autoplayTimeout: 2000,
+    autoWidth: false,
+    slideBy: 1,
+    items:1
+  });
+
+  // Off-Canvas Navigation
+  // jQuery('.menu--main').prepend('<a href="javascript:void(0);" class="aws-close-btn">X</a>');
+
+  jQuery('.toggle-menu').on('click', function(event) {
+    // if (jQuery('.menu-toggle-btn').hasClass('close-menu')) {
+    //   jQuery('.menu-toggle-btn').removeClass('close-menu')
+    // }
+    // else {
+    //   jQuery('.menu-toggle-btn').addClass('close-menu')
+    // }
+
+    event.stopPropagation();
+    jQuery('.site-logo').toggleClass('fade-in');
+  });
+  jQuery('.aws-close-btn').click(function() {
+    // event.preventDefault();
+    jQuery(this).parent().removeClass('fade-in');
+  });
+
+
+  // jQuery(document).click(function() {
+  //   // alert('test');
+  //   jQuery('.menu-offset-left').removeClass('fade-in');
+  // });
+
   
-//   jQuery('.menu-parent, .submenu-parent').prepend('<div class="cart-arrow"></div>');
-//   jQuery('.cart-arrow').click(function() {
-//     // alert('heee');
-//     jQuery(this).parent('.menu-parent').children('.submenu').slideToggle();
-//     jQuery(this).parent('.submenu-parent').children('.submenu-level').slideToggle();
-//   })
+
+  // Submenu visible on click
   
+  jQuery('.menu-item--expanded, .submenu-parent').prepend('<div class="cart-arrow"></div>');
+  jQuery('.cart-arrow').click(function() {
+    // alert('heee');
+    jQuery(this).parent('.menu-item--expanded').children('.menu').slideToggle();
+    // jQuery(this).parent('.menu').children('.menu-item').slideToggle();
+  });
 
-//   // Smooth Scrolling
-//   $('a[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: target.offset().top
-//         }, 1500);
-//         return false;
-//       }
-//     }
-//   });
+  // Smooth Scrolling
+  jQuery('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        jQuery('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1500);
+        return false;
+      }
+    }
+  });
 
-// });
+  // Dropdown Box
+  jQuery('span').click(function() {
+    jQuery('.drop-down-main').toggleClass('active-dropdown');
+  });
+
+  //  Social media for device
+  jQuery('.share-icon').click(function(){
+    jQuery(this).parent('.social-media-main').toggleClass('social-icon-in')
+  });
+
+
+});
